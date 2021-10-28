a91b4e4938788a56f6a1418d0db2f392066a9f78
bit-wise <bit-wise@users.noreply.github.com>
2021-10-28 11:59:31 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,92 +1,85 @@
-let canvas, img, X, Y, rangeX, rangeY, C, T;
-const W = window.innerWidth;
+const inp = 'sample3.jpg';
+const outp = ['living.color', 'jpg'];
+const W = 1920 * 2;
 const H = W * (3 / 4);
-const W2 = W / 2;
-const H2 = H / 2;
 const range = Math.min(W, H);
+const inc = 0.01;
+const rad = W / 50;
+const rst = Math.round(rad / inc);
+const M = 1024;
+const M2 = M / 2;
+let canvas, img, X, Y, rX, rY, C, J, nR, xR, nG, xG, nB, xB, SW;
+let I = 0;
+let L = 0;
+let prt = false;
+let xyMap = [];
 
 function preload() {
-    img = loadImage('sample3.jpg');
+    img = loadImage(inp);
 }
 
 function setup() {
-    // pixelDensity(1);
+    pixelDensity(1);
     image(img, 0, 0);
     canvas = createCanvas(W, H);
     X = random(0, width);
     Y = random(0, height);
 }
 
-let I = 0;
-let J = 1;
-let minR = 0;
-let maxR = 255;
-let minG = 0;
-let maxG = 255;
-let minB = 0;
-let maxB = 255;
-let inc = 0.01;
-let rad = 50;
-let rst = Math.round(rad / inc);
-let prt = false;
-let j = 1;
-let K = 0;
-let L = 0;
-let D = 0.0005
-let O = 2;
-
 function draw() {
     if (I == 0) {
         image(img, 0, 0, W, H);
     }
     if (I == rst && prt) {
-        saveCanvas('living.color', 'jpg');
+        saveCanvas(outp[0], outp[1]);
     }
     if (I > rst * 2) {
         I = 0;
-        J = 1;
-        K = 0;
         L = 0;
     } else if (I > rst) {
-        J -= inc;
-        K -= inc / 20;
         L--;
     } else {
-        J += inc;
-        K += inc / 20;
         L++;
     }
     I++;
-    for (let i = 0; i < 1000; i++) {
-        rangeX = random(-range, range);
-        rangeY = random(-range, range);
-        X += rangeX;
-        Y += rangeY;
+    nR = 128; xR = 128; nG = 128; xG = 128; nB = 128; xB = 128;
 
-        if (X > width) { X = X - width; }
-        if (Y > height) { Y = Y - height; }
-        if (X < 0) { X = width + X; }
-        if (Y < 0) { Y = height + Y; }
+    SW = rad / (L * inc + 1);
+    J = map(L, 0, rst, 0, 1);
+    xyMap = [];
+
+    for (let i = 0; i < M; i++) {
+        rX = random(-range, range);
+        rY = random(-range, range);
+        X += rX;
+        Y += rY;
+        X = round(X);
+        Y = round(Y);
+
+        if (X >= width) { X = X - width; }
+        if (Y >= height) { Y = Y - height; }
+        if (X <= 0) { X = width + X; }
+        if (Y <= 0) { Y = height + Y; }
 
         C = get(X, Y);
-        //T = C[0] + C[1] + C[2];
-        minR = min(C[0], minR + D);
-        maxR = max(C[0], maxR - D);
-        minG = min(C[1], minG + D);
-        maxG = max(C[1], maxG - D);
-        minB = min(C[2], minB + D);
-        maxB = max(C[2], maxB - D);
-
-        strokeWeight(rad / J + random(0, j));
-        stroke(
-            round(map(C[0] + random(-j - K, j + K), minR - O, maxR + O, 0, 255)),
-            round(map(C[1] + random(-j - K, j + K), minG - O, maxG + O, 0, 255)),
-            round(map(C[2] + random(-j - K, j + K), minB - O, maxB + O, 0, 255)),
-            //round(map(L, 0, rst / rad, 0, 255))
-        );
-        point(X + 0, Y + 0);
+        xyMap.push({ x: X, y: Y, 
+            r: C[0] + random(-J, J), g: C[1] + random(-J, J), b: C[2] + random(-J, J) });
     }
-}
-function mouseReleased() {
-    saveCanvas('living.color', 'jpg');
+    xyMap.map(m => {
+        nR = min(m.r, nR);
+        xR = max(m.r, xR);
+        nG = min(m.g, nG);
+        xG = max(m.g, xG);
+        nB = min(m.b, nB);
+        xB = max(m.b, xB);
+    });
+    xyMap.map(m => {
+        strokeWeight(SW);
+        stroke(
+            round(map(m.r, nR, xR, 0, 255)),
+            round(map(m.g, nG, xG, 0, 255)),
+            round(map(m.b, nB, xB, 0, 255)),
+        );
+        point(m.x, m.y);
+    });
 }
