e908fc506f6e4c4b953ef60d4e0221af779c1650
bit-wise <bit-wise@users.noreply.github.com>
2021-10-28 14:10:15 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,8 +7,7 @@ const inc = 0.01;
 const rad = W / 50;
 const rst = Math.round(rad / inc);
 const M = 1024;
-const M2 = M / 2;
-let canvas, img, X, Y, rX, rY, C, J, nR, xR, nG, xG, nB, xB, SW;
+let canvas, img, X, Y, C, J, nR, xR, nG, xG, nB, xB, SW, R;
 let I = 0;
 let L = 0;
 let prt = false;
@@ -22,39 +21,27 @@ function setup() {
     pixelDensity(1);
     image(img, 0, 0);
     canvas = createCanvas(W, H);
-    X = random(0, width);
-    Y = random(0, height);
+    X = round(random(0, width));
+    Y = round(random(0, height));
 }
 
 function draw() {
-    if (I == 0) {
-        image(img, 0, 0, W, H);
-    }
-    if (I == rst && prt) {
-        saveCanvas(outp[0], outp[1]);
-    }
-    if (I > rst * 2) {
-        I = 0;
-        L = 0;
-    } else if (I > rst) {
-        L--;
-    } else {
-        L++;
-    }
+    if (I == 0) { image(img, 0, 0, W, H); }
+    if (I == rst && prt) { saveCanvas(outp[0], outp[1]); }
+    if (I > rst * 2) { I = 0; L = 0; } else
+        if (I > rst) { L--; }
+        else { L++; }
     I++;
     nR = 128; xR = 128; nG = 128; xG = 128; nB = 128; xB = 128;
 
     SW = rad / (L * inc + 1);
-    J = map(L, 0, rst, 0, 1);
+    J = map(L, 0, rst, 0, 10);
+    R = random(-J, J);
     xyMap = [];
 
     for (let i = 0; i < M; i++) {
-        rX = random(-range, range);
-        rY = random(-range, range);
-        X += rX;
-        Y += rY;
-        X = round(X);
-        Y = round(Y);
+        X += round(random(-range, range));
+        Y += round(random(-range, range));
 
         if (X >= width) { X = X - width; }
         if (Y >= height) { Y = Y - height; }
@@ -62,16 +49,12 @@ function draw() {
         if (Y <= 0) { Y = height + Y; }
 
         C = get(X, Y);
-        xyMap.push({ x: X, y: Y, 
-            r: C[0] + random(-J, J), g: C[1] + random(-J, J), b: C[2] + random(-J, J) });
+        xyMap.push({ x: X, y: Y, r: C[0] + R, g: C[1] + R, b: C[2] + R });
     }
     xyMap.map(m => {
-        nR = min(m.r, nR);
-        xR = max(m.r, xR);
-        nG = min(m.g, nG);
-        xG = max(m.g, xG);
-        nB = min(m.b, nB);
-        xB = max(m.b, xB);
+        nR = min(m.r, nR); xR = max(m.r, xR);
+        nG = min(m.g, nG); xG = max(m.g, xG);
+        nB = min(m.b, nB); xB = max(m.b, xB);
     });
     xyMap.map(m => {
         strokeWeight(SW);
