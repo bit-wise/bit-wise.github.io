1727f1132a7fad085a110646ea1b1aed8f858129
bit-wise <bit-wise@users.noreply.github.com>
2021-10-29 10:46:49 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,14 +4,16 @@ const W = 1920 * 2;
 const H = W * (3 / 4);
 const range = Math.min(W, H);
 const inc = 0.01;
-const rad = 50;
+const rad = 75;
 const rst = Math.round(rad / inc);
 const M = 1024;
 let canvas, img, X, Y, C, J, nR, xR, nG, xG, nB, xB, SW, R;
+let pI = 0;
 let I = 0;
 let L = 0;
 let prt = false;
 let xyMap = [];
+let pxMap = [];
 
 function preload() {
     img = loadImage(inp);
@@ -21,12 +23,14 @@ function setup() {
     pixelDensity(1);
     image(img, 0, 0);
     canvas = createCanvas(W, H);
-    X = round(random(0, width));
-    Y = round(random(0, height));
+    for (let x = 0; x < W; x++) { for (let y = 0; y < H; y++) { pxMap.push({ x, y }); } }
+    pxMap = shuffle(pxMap);
+    X = pxMap[0].x;
+    Y = pxMap[0].y;
 }
 
 function draw() {
-    if (I == 0) { image(img, 0, 0, W, H); }
+    if (I == 0) { image(img, -10, -10, W + 10, H + 10); }
     if (I == rst && prt) { saveCanvas(outp[0], outp[1]); }
     if (I > rst * 2) { I = 0; L = 0; } else
         if (I > rst) { L--; }
@@ -34,19 +38,16 @@ function draw() {
     I++;
     nR = 128; xR = 128; nG = 128; xG = 128; nB = 128; xB = 128;
 
-    SW = map(rad / (L * inc + 1), rad, 1, rad * 5, 1);
-    J = map(L, 0, rst, 0, 10);
+    SW = map(rad / (L * inc + 1), rad, 1, 250, 1);
+    J = map(L, 0, rst, 0, 4);
     R = random(-J, J);
     xyMap = [];
 
     for (let i = 0; i < M; i++) {
-        X += round(random(-range, range));
-        Y += round(random(-range, range));
-
-        if (X >= width) { X = X - width; }
-        if (Y >= height) { Y = Y - height; }
-        if (X <= 0) { X = width + X; }
-        if (Y <= 0) { Y = height + Y; }
+        pI++;
+        if (pI >= pxMap.length) { pI = 0; }
+        X = pxMap[pI].x;
+        Y = pxMap[pI].y;
 
         C = get(X, Y);
         xyMap.push({ x: X, y: Y, r: C[0] + R, g: C[1] + R, b: C[2] + R });
