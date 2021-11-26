89059e29e39a1441a86debc32cda003ce1f7112e
bit-wise <bit-wise@users.noreply.github.com>
2021-11-26 07:10:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,14 @@
-const inp = 'sample3.jpg';
 const outp = ['living.color', 'jpg'];
 const rst = 10000;
-const fac = 4;
-const W = 1920 * 2; const W4 = W / 4; const H = W * (3 / 4); const os = Math.floor(fac / 2); const sw = Math.pow(fac, 2); const Wos = (W - os) / fac; const Hos = (H - os) / fac;
-let canvas, img, C, P; let M = W4 / 2; let I = 0; let prt = false; let xyMap = []; pI = 0; let pxMap = [];
-function preload() { img = loadImage(inp); }
+const fac = 6;
+const W = 1920 * 2; const W4 = W / 4; const H = W * (3 / 4); const os = 1; const sw = Math.pow(fac, 2); const Wos = (W - os * 2) / fac; const Hos = (H - os * 2) / fac;
+let canvas, img, C, P; let M = W4 / 2; let I = 0; let E = 0; let prt = false; let xyMap = []; pI = 0; let pxMap = [];
 function setup() {
     pixelDensity(1);
     canvas = createCanvas(W, H);
     for (let x = os; x < Wos; x++) { for (let y = os; y < Hos; y++) { pxMap.push({ x: x * fac, y: y * fac, w: round(random(fac, sw)) }); } }
     pxMap = shuffle(pxMap);
     X = pxMap[0].x; Y = pxMap[0].y;
-    image(img, 0, 0, W, H);
 }
 function draw() {
     I++; if (I == pxMap.length - 1 && prt) { saveCanvas(outp[0], outp[1]); I = 0; }
@@ -20,7 +17,8 @@ function draw() {
     for (let i = 0; i < M; i++) {
         pI++; if (pI >= pxMap.length) { pI = 0; }; P = pxMap[pI];
         C = get(P.x, P.y);
-        xyMap.push({ x: P.x, y: P.y, r: C[0], g: C[1], b: C[2], w: P.w });
+        if (C[3] > 0) { xyMap.push({ x: P.x, y: P.y, r: C[0], g: C[1], b: C[2], w: P.w }); }
+        else { xyMap.push({ x: P.x, y: P.y, r: random(0, 255), g: random(0, 255), b: random(0, 255), w: P.w }); }
     }
     xyMap.map(m => { strokeWeight(m.w); stroke(m.r, m.g, m.b, 255); point(m.x, m.y); });
 }
