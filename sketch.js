0a604b4b91237a80fe753f415a86aab66fde10bb
bit-wise <bit-wise@users.noreply.github.com>
2021-11-28 04:49:29 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,33 @@
 const outp = ['living.color', 'jpg'];
 const rst = 10000;
-const fac = 3;
-const W = 1920 * 1; const W4 = W / 4; const H = W * (3 / 4); const os = 1; const sw = Math.pow(fac, 2); const Wos = (W - os * 2) / fac; const Hos = (H - os * 2) / fac;
-let canvas, img, C, P; let M = W4 / 2; let I = 0; let E = 0; let prt = false; let xyMap = []; pI = 0; let pxMap = [];
+const fac = 1;
+const W = 1920 * 1; const H = W * (3 / 4); const WM = W * 1.50; const os = 1; const Wos = (W - os * 2) / fac; const Hos = (H - os * 2) / fac;
+let canvas, img, C, P, pos, Cc; let F = 4; let Ca = [[F, -F], [0, -F], [-F, -F], [F, 0], [-F, 0], [F, F], [0, F], [-F, F]]; let M = WM; let I = 0; let E = 0; let prt = false; let xyMap = []; pI = 0; let pxMap = [];
 function setup() {
     pixelDensity(2);
     canvas = createCanvas(W, H);
-    for (let x = os; x < Wos; x++) { for (let y = os; y < Hos; y++) { pxMap.push({ x: x * fac, y: y * fac, w: round(random(fac, sw)) }); } }
+    // for (let x = os; x < Wos; x++) { for (let y = os; y < Hos; y++) { pxMap.push({ x: x * fac, y: y * fac, w: round(random(0, 1)) * 2 + 3 }); } }
+    for (let x = os; x < Wos; x++) { for (let y = os; y < Hos; y++) { pxMap.push({ x: x * fac, y: y * fac, w: fac + 2 }); } }
     pxMap = shuffle(pxMap);
     X = pxMap[0].x; Y = pxMap[0].y;
+    frameRate(0.1 * 10); rend();
 }
-function draw() {
+function draw() { rend(); }
+function rend() {
     I++; if (I == pxMap.length - 1 && prt) { saveCanvas(outp[0], outp[1]); I = 0; }
     xyMap = [];
-    if (frameRate() > 24 && M < W4) { M++ } else { M-- }
+    if (M < WM) { M++ } else { M-- }
     for (let i = 0; i < M; i++) {
-        pI++; if (pI >= pxMap.length) { pI = 0; pxMap = shuffle(pxMap);}; P = pxMap[pI];
+        pI++; if (pI >= pxMap.length) { pI = 0; pxMap = shuffle(pxMap); }; P = pxMap[pI];
         C = get(P.x, P.y);
-        if (C[3] > 0) { xyMap.push({ x: P.x, y: P.y, r: C[0], g: C[1], b: C[2], w: P.w }); }
+        if (C[3] > 0) { xyMap.push({ x: P.x, y: P.y, r: C[0], g: C[1], b: C[2], w: P.w + grow(C, P) }); }
         else { xyMap.push({ x: P.x, y: P.y, r: round(random(0, 255)), g: round(random(0, 255)), b: round(random(0, 255)), w: P.w }); }
     }
     xyMap.map(m => { strokeWeight(m.w); stroke(m.r, m.g, m.b); point(m.x, m.y); });
 }
+function grow(C, P) {
+    pos = 0; if (P.x < 0 || P.x > W || P.y < 0 || P.y > H) { return pos; }
+    Ca.map(c => { Cc = get(P.x + c[0], P.y + c[1]); if (Cc[0] == C[0] && Cc[1] == C[1] && Cc[2] == C[2]) { pos += 1; } });
+    return pos;
+}
+prt = true;
