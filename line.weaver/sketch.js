bc555b8b94ea33472925fb97dd56874228333af9
bit-wise <bit-wise@users.noreply.github.com>
2021-12-25 21:29:17 -0500

    Add files via upload

--- line.weaver/sketch.js
+++ line.weaver/sketch.js
@@ -17,8 +17,8 @@ function setup() {
   frameRate(3);
   createCanvas(W, H);
   strokeWeight(1);
-  for (let x = 1; x < W1; x++) { rx.push(x); } rx = shuffle(rx);
-  for (let y = 1; y < H1; y++) { ry.push(y); } ry = shuffle(ry);
+  for (let x = 1; x < W1; x++) { if (rand(1) > 0.75) { rx.push(x); } } rx = shuffle(rx);
+  for (let y = 1; y < H1; y++) { if (rand(1) > 0.75) { ry.push(y); } } ry = shuffle(ry);
   for (let i = 1; i < W1; i++) { LR.push(i); }
   for (let i = W1; i > 1; i--) { RL.push(i); }
   for (let i = 1; i < H1; i++) { TB.push(i); }
@@ -26,10 +26,11 @@ function setup() {
 }
 
 function draw() {
-  let S = xyi % 2 == 0 ? ry[ryi++] : rx[rxi++];
+  let C = xyi % 2 == 0 && ryi < ry.length;
+  let S = C ? ry[ryi++] : rx[rxi++];
   let B = false;
   let L = [];
-  if (xyi % 2 == 0) {
+  if (C) {
     if (xyi == 0) {
       LR.map((d) => { B = get(d, S)[3] > 0 ? !B : B; if (!B) { L.push([d, S]); } });
     } else {
@@ -47,5 +48,5 @@ function draw() {
     point(l[0], l[1]);
   });
   xyi++;
-  if (xyi >= ry.length + ry.length) { noLoop(); }
+  if (xyi >= rx.length + ry.length) { noLoop(); console.log('end') }
 }
