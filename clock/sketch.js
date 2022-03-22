ba2de8c61fe81f73108206255865fdab9ea510c3
bit-wise <bit-wise@users.noreply.github.com>
2022-03-22 16:19:37 -0400

    Update sketch.js

--- clock/sketch.js
+++ clock/sketch.js
@@ -4,17 +4,23 @@ const pad = Math.max(W, H) * 0.1;
 const pad2 = pad / 2;
 const pad4 = pad / 4;
 const pad8 = pad / 8;
-const m_s = 1000;
-const m_m = 1000 * 60;
-const m_h = 1000 * 60 * 60;
-const m_d = 1000 * 60 * 60 * 24;
-const m_w = 1000 * 60 * 60 * 24 * 7;
-const m_n = 1000 * 60 * 60 * 24 * 7 * 4;
-const m_y = 1000 * 60 * 60 * 24 * 365;
+const M = [
+  // 1000, // secs
+  1000 * 60.007610352000078535, // mins
+  1000 * 3600.4566211200049111, // hours
+  1000 * 86410.958906880114228, // days
+  1000 * 604876.71234816079959, // weeks
+  1000 * 2628336.2137829000000, // months
+  1000 * 31540000.001011241227, // years
+  1000 * 315400000.01011240482, // decade
+  1000 * 3154000000.1011242867, // century
+];
+const ML = M.length;
 const fr = 30;
-const bch = 50;
+const bch = 20;
 let up = 0;
-let d = {org:0,mil:0};
+let D;
+let d = { org: 0, mil: 0 };
 
 function setup() {
   frameRate(fr);
@@ -29,39 +35,33 @@ function setup() {
 }
 
 function draw() {
-  d.org = Date.now();
-  d.mil = Math.floor(d.org);
-  d.sec = Math.floor(d.org / m_s);
-  d.min = Math.floor(d.org / m_m);
-  d.hor = Math.floor(d.org / m_h);
-  d.day = Math.floor(d.org / m_d);
-  d.wek = Math.floor(d.org / m_w);
-  d.mnt = Math.floor(d.org / m_n);
-  d.yer = Math.floor(d.org / m_y);
-  rng.I = d.min;
+  D = Date.now();
+  rng.I = Math.floor(D / M[0]);
+
   push();
   fill(200);
   noStroke();
   rect(0, H - pad2, W, H);
   pop();
-  push();
-  stroke(0, 0, 0, 64);
-  noFill();
-  arc(pad4 * 6 + pad8 * 5, H - pad4, pad4, pad4, 0, TWO_PI);
-  arc(pad4 * 5 + pad8 * 4, H - pad4, pad4, pad4, 0, TWO_PI);
-  arc(pad4 * 4 + pad8 * 3, H - pad4, pad4, pad4, 0, TWO_PI);
-  arc(pad4 * 3 + pad8 * 2, H - pad4, pad4, pad4, 0, TWO_PI);
-  arc(pad4 * 2 + pad8 * 1, H - pad4, pad4, pad4, 0, TWO_PI);
-  arc(pad4 * 1 + pad8 * 0, H - pad4, pad4, pad4, 0, TWO_PI);
-  stroke(0);
-  fill(0);
-  arc(pad4 * 6 + pad8 * 5,H - pad4,pad4,pad4,-HALF_PI,((d.org % m_m) / m_m) * (TWO_PI * 1) - HALF_PI);
-  arc(pad4 * 5 + pad8 * 4,H - pad4,pad4,pad4,-HALF_PI,((d.org % m_h) / m_h) * (TWO_PI * 1) - HALF_PI);
-  arc(pad4 * 4 + pad8 * 3,H - pad4,pad4,pad4,-HALF_PI,((d.org % m_d) / m_d) * (TWO_PI * 1) - HALF_PI);
-  arc(pad4 * 3 + pad8 * 2,H - pad4,pad4,pad4,-HALF_PI,((d.org % m_w) / m_w) * (TWO_PI * 1) - HALF_PI);
-  arc(pad4 * 2 + pad8 * 1,H - pad4,pad4,pad4,-HALF_PI,((d.org % m_n) / m_n) * (TWO_PI * 1) - HALF_PI);
-  arc(pad4 * 1 + pad8 * 0,H - pad4,pad4,pad4,-HALF_PI,((d.org % m_y) / m_y) * (TWO_PI * 1) - HALF_PI);
-  pop();
+
+  M.map((m, i) => {
+    push();
+    stroke(0, 0, 0, 64);
+    noFill();
+    arc(pad4 * (ML - i) + pad8 * (ML - 1 - i), H - pad4, pad4, pad4, 0, TWO_PI);
+    stroke(0);
+    fill(0);
+    arc(
+      pad4 * (ML - i) + pad8 * (ML - 1 - i),
+      H - pad4,
+      pad4,
+      pad4,
+      -HALF_PI,
+      ((D % m) / m) * (TWO_PI * 1) - HALF_PI
+    );
+    pop();
+  });
+
   for (let i = 0; i < bch; i++) {
     stroke(0, 0, 0, 8);
     line(
